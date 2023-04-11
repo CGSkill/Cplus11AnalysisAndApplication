80084dfbf715cf1afd02a5730ac4b46cb214fc37
MorningLight <545318542@qq.com>
2023-04-11 14:35:20 +0800

    add

--- Test_Cplus11_ForceRightValue_3_3_6/main.cpp
+++ Test_Cplus11_ForceRightValue_3_3_6/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+class Moveable
+{
+public:
+    Moveable() {}
+    ~Moveable() {delete i;}
+    Moveable(const Moveable &m):i (new int (*m.i)) {}
+    Moveable(Moveable &&m):i(m.i){
+        m.i = nullptr;
+        cout << m.i << endl;
+    }
+
+    int * i;
+private:
+};
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Moveable a;
+    Moveable c(move(a));
+
     return 0;
 }
--- Test_Cplus11_explicit_3_4_1/main.cpp
+++ Test_Cplus11_explicit_3_4_1/main.cpp
@@ -1,9 +1,71 @@
-#include <iostream>
+////
+///
+///
+///
+////
 
+
+
+#include <iostream>
 using namespace std;
+
+struct Rational1
+{
+    Rational1(int n = 0, int d = 1):num(n),den(d) {
+        cout<< __func__ << ":" << num << "/" << den << endl;
+    }
+
+    int num;
+    int den;
+};
+
+
+struct Rational2
+{
+
+    ///
+    /// \brief Rational2
+    /// \param n
+    /// \param d
+    /// 普通构造函数能够被隐式调用。而explicit构造函数只能被显式调用
+    ///
+    explicit Rational2(int n = 0, int d = 1):num(n),den(d) {
+        cout<< __func__ << ":"<< num << "/" << den << endl;
+    }
+
+    int num;
+    int den;
+};
+
+
+void Display1(Rational1 ra){
+    cout << "1-Numerator: " << ra.num  << " Denominator:  " << ra.den << endl;
+}
+
+
+void Display2(Rational2 ra){
+    cout << "2-Numerator: " << ra.num  << " Denominator:  " << ra.den << endl;
+}
+
 
 int main()
 {
+
+    Rational1 r1_1 = 11;
+    Rational1 r1_2(12);
+
+    //Rational2 r2_1 = 21; //无法通过编译  explicit构造函数只能被显式调用
+    Rational2 r2_2(22);
+
+
+    Display1(1);
+    //Display2(1); //无法通过编译 隐式转换
+
+    Display2(Rational2(2));
+
+
+
+
     cout << "Hello World!" << endl;
     return 0;
 }
